0d4a3e9427c3151f3fe0045423fe0546ce09b6b9
Harsh Bhamore <harsh.bhamore3@gmail.com>
2019-10-25 13:48:20 +0530

    Update 1023B.cpp

--- 1023B.cpp
+++ 1023B.cpp
@@ -22,5 +22,28 @@ int main()
             ans=n-x+1;
         }
     }
+    for(int i=0;i<s.size()-1;i++)
+    {
+        if((s[i]=='1'&&s[i+1]=='0'))
+        {
+            swap(s[i],s[i+1]);
+            countm++;
+        }
+        if((s[i]=='2'&&s[i+1]=='1'))
+        {
+            swap(s[i],s[i+1]);
+            countm++;
+        }
+    }
+    for(int i=0;i<n;i++)
+        
+       {
+       bhams[i] = b1[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            cin>>b1[i];
+            cin>>b2[i];
+        }
     cout<<ans<<endl;
 }
